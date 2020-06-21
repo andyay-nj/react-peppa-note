127fb0464806052fb3eb2730eea1dd6e9b204f58
andyay-nj <knajaeng@gmail.com>
2020-06-21 14:42:10 +0900

    Updates

---
+++ precache-manifest.0be6246bfcd5736ea83b260ad09a239e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8373383d107ebfb8df2a6fdc2a80854f",
+    "url": "/andyay-nj/react-peppa-note/index.html"
+  },
+  {
+    "revision": "b08d5f5cecf3e79920b2",
+    "url": "/andyay-nj/react-peppa-note/static/css/main.88fb3cd6.chunk.css"
+  },
+  {
+    "revision": "0e021d34abd28f025e2d",
+    "url": "/andyay-nj/react-peppa-note/static/js/2.1e458b10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/andyay-nj/react-peppa-note/static/js/2.1e458b10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b08d5f5cecf3e79920b2",
+    "url": "/andyay-nj/react-peppa-note/static/js/main.982c62c2.chunk.js"
+  },
+  {
+    "revision": "0a755aed3ee4e7a532d8",
+    "url": "/andyay-nj/react-peppa-note/static/js/runtime-main.b6355686.js"
+  },
+  {
+    "revision": "a8004c868a41c0ec0de25612dd9c0904",
+    "url": "/andyay-nj/react-peppa-note/static/media/peppa6.a8004c86.png"
+  },
+  {
+    "revision": "a9bd1d16b8d5c60ded229a1c1c051d7f",
+    "url": "/andyay-nj/react-peppa-note/static/media/peppa7.a9bd1d16.jpg"
+  }
+]);
